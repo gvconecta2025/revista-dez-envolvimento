ed68e24dfca5c8f8e08d004ae57903f7578e17a0
gvconecta2025 <gvconecta2025@gmail.com>
2026-05-19 09:39:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 
-// Configuração básica do Firebase dentro do SW
+// Configuração injetada com os seus dados reais
 firebase.initializeApp({
-    apiKey: "SUA_API_KEY_AQUI", // Use os mesmos dados do seu firebase-config.js
-    projectId: "SEU_PROJECT_ID",
-    messagingSenderId: "SEU_SENDER_ID"
+    apiKey: "AIzaSyD4Ag5LEioFxlseyQmWWu5ov64sgK04F2M",
+    projectId: "revista-dez-envolvimento",
+    messagingSenderId: "845251441976",
+    appId: "1:845251441976:web:dc41c681ed0f3d475a3dcd"
 });
 
 const messaging = firebase.messaging();
 
-// Exibe a notificação na tela quando o site estiver em segundo plano
+// Lógica para receber a notificação em segundo plano
 messaging.onBackgroundMessage((payload) => {
+  console.log('Notificação recebida em segundo plano: ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icons/icon-192.png'
+    icon: '/icons/icon-192.png',
+    badge: '/icons/icon-192.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
